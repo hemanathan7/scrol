d594a679aef1b3d2f3f4ff90358c93c73fc1619c
hemanathan7 <126547979+hemanathan7@users.noreply.github.com>
2024-01-13 01:21:48 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import ImageOne from "./components/ImageOne";
+import ImageThree from "./components/ImageThree";
+import ImageTwo from "./components/ImageTwo";
+import TextBox from "./components/TextBox";
+
+
+function App() {
+  return (
+    <div>
+    <ImageOne/>
+    <TextBox/>
+    <ImageTwo/>
+    <TextBox/>
+    <ImageThree/>
+    <TextBox/>
+    </div>
+  );
+}
+
+export default App;
